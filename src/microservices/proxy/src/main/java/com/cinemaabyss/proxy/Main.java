adfe451eec87f64bc31091a57922904655ad096f
Темненков Кирилл Дмитриевич <kirill.temnenkov@mts.ru>
2025-10-17 19:29:33 +0300

    initial

---
+++ src/microservices/proxy/src/main/java/com/cinemaabyss/proxy/Main.java
@@ -0,0 +1,30 @@
+package com.cinemaabyss.proxy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static spark.Spark.awaitInitialization;
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        // Читаем порт из переменной окружения, по умолчанию 8000
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8000"));
+        
+        port(port);
+        
+        logger.info("Starting Proxy Service on port {}", port);
+        
+        // Пока просто Hello World на любой запрос
+        get("/*", (req, res) -> {
+            logger.info("Received request: {} {}", req.requestMethod(), req.pathInfo());
+            return "Hello from Proxy! 👋";
+        });
+        
+        awaitInitialization();
+        logger.info("Proxy Service started successfully!");
+    }
+}
